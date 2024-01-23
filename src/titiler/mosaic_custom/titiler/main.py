61664e314f203d3325b527c5f5187b538c8331b6
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2024-01-23 15:00:09 +0700

    update custom backend [titiler] v2

--- src/titiler/mosaic_custom/titiler/main.py
+++ src/titiler/mosaic_custom/titiler/main.py
@@ -15,10 +15,10 @@ app = FastAPI(
     title="Mosaic Custom",
     description="Custom mosaic backend.",
     version="0.1.0",
-    docs_url="/custom/docs",
-    redoc_url="/custom/redoc",
-    openapi_url="/custom/openapi.json",
+    # docs_url="/custom/docs",
+    # redoc_url="/custom/redoc",
+    # openapi_url="/custom/openapi.json",
 )
-app.include_router(mosaic.router, prefix="/custom", tags=["custom"])
+app.include_router(mosaic.router)
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 add_exception_handlers(app, MOSAIC_STATUS_CODES)
