319536e7bf21750a8c33dd4a2f0411870580b17c
watcharapon weeraborirak <watcharapon.w@thaicom.net>
2024-01-24 15:42:09 +0700

    update main.py

--- src/titiler/mosaic_custom/titiler/main.py
+++ src/titiler/mosaic_custom/titiler/main.py
@@ -14,17 +14,17 @@ app = FastAPI(
     title="Mosaic Custom",
     description="Custom mosaic backend.",
     version="0.1.0",
-    # docs_url="/custom/docs",
-    # redoc_url="/custom/redoc",
-    # openapi_url="/custom/openapi.json",
+    docs_url="/custom/docs",
+    redoc_url="/custom/redoc",
+    openapi_url="/custom/openapi.json",
 )
-app.include_router(mosaic.router)
+app.include_router(mosaic.router, prefix="/custom")
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 add_exception_handlers(app, MOSAIC_STATUS_CODES)
 
 
 # Health Check
-@app.get("/healthz", tags=["Health Check"])
+@app.get("/custom/healthz", tags=["Health Check"])
 def ping():
     """Health check."""
     return {"ping": "pong!"}
